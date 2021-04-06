2424642a1add15dcd3806c7e526f5a8b5e5306e0
joseph Evangelista <oootep123@gmail.com>
2021-04-06 11:27:36 -0700

    price updating

--- app.js
+++ app.js
@@ -15,29 +15,41 @@ let itemTax1 = document.querySelector(".itemT1");
 let itemTax2 = document.querySelector(".itemT2");
 let startingTax1 = itemTax1.innerHTML;
 let startingTax2 = itemTax2.innerHTML;
-let totalPrice = document.querySelector(".total-fee");
+let totalPrice = document.querySelector(".totalN");
 let startingTotal = totalPrice.innerHTML;
+let totalI;
 counter1 = 1;
 counter2 = 1;
 // Event Listeners
 function adder(v) {
     if(v===item1){
         counter1 += 1;
-        v.innerHTML = counter1;     
+        v.innerHTML = counter1; 
+        newPrice = parseFloat(itemPrice1.innerHTML).toFixed(2);
         itemPrice1.innerHTML = 
         parseFloat((startingPrice1) * counter1).toFixed(2);
         itemTax1.innerHTML = 
         parseFloat((startingTax1) * counter1).toFixed(2);
-        totalPrice.innerHTML = parseFloat(itemPrice1.innerHTML + 148.99).toFixed(2);
+        totalPrice.innerHTML = (parseFloat(startingTotal)+ parseFloat(newPrice)).toFixed(2);
+
+        // console.log(parseFloat(startingTotal)+ parseFloat(newPrice));
+        // console.log(totalPrice.innerHTML)
+
     }else{
         counter2 += 1;
         v.innerHTML = counter2;
+        newPrice1 = parseFloat(itemPrice2.innerHTML).toFixed(2);
         itemPrice2.innerHTML = 
         parseFloat((startingPrice2) * counter2).toFixed(2);
         itemTax2.innerHTML = 
         parseFloat((startingTax2) * counter2).toFixed(2);
+        // return itemPrice2;
+        totalPrice.innerHTML = (parseFloat(startingTotal)+ parseFloat(newPrice1)).toFixed(2);
+        
+        // console.log(parseFloat(startingTotal)+ parseFloat(newPrice1));
+        // console.log(totalPrice.innerHTML)
     }
-
+    let totalI;
 }
 
 function subtracter(v) {
@@ -58,11 +70,6 @@ function subtracter(v) {
     }
 }
 
-
-// function updatePrices(v) {
-
-//         document.querySelector(".itemP1").innerHTML = "100.99";
-// }
 
 // Event Handlers
 addBtn1.addEventListener("click", function(){
