a08725433e062b671e022fe494dbe4872b0c9aa8
joseph Evangelista <oootep123@gmail.com>
2021-04-05 23:26:50 -0700

    price updating

--- app.js
+++ app.js
@@ -1,38 +1,73 @@
+// Buttons
 const addBtn1 = document.querySelector(".add1");
 const addBtn2 = document.querySelector(".add2");
 const minusBtn1 = document.querySelector(".minus1");
 const minusBtn2 = document.querySelector(".minus2");
+
+// Html Elements 
 let item1 = document.querySelector(".item1");
 let item2 = document.querySelector(".item2");
+let itemPrice1 = document.querySelector(".itemP1");
+let itemPrice2 = document.querySelector(".itemP2");
+let startingPrice1 = itemPrice1.innerHTML;
+let startingPrice2 = itemPrice2.innerHTML;
+let itemTax1 = document.querySelector(".itemT1");
+let itemTax2 = document.querySelector(".itemT2");
+let startingTax1 = itemTax1.innerHTML;
+let startingTax2 = itemTax2.innerHTML;
+let totalPrice = document.querySelector(".total-fee");
+let startingTotal = totalPrice.innerHTML;
 counter1 = 1;
 counter2 = 1;
-
-// addBtn1.addEventlistener("click", adder());
-// addBtn2.addEventlistener("click",add(item2));
-
+// Event Listeners
 function adder(v) {
     if(v===item1){
         counter1 += 1;
-        v.innerHTML = counter1
+        v.innerHTML = counter1;     
+        itemPrice1.innerHTML = 
+        parseFloat((startingPrice1) * counter1).toFixed(2);
+        itemTax1.innerHTML = 
+        parseFloat((startingTax1) * counter1).toFixed(2);
+        totalPrice.innerHTML = parseFloat(itemPrice1.innerHTML + 148.99).toFixed(2);
     }else{
         counter2 += 1;
         v.innerHTML = counter2;
+        itemPrice2.innerHTML = 
+        parseFloat((startingPrice2) * counter2).toFixed(2);
+        itemTax2.innerHTML = 
+        parseFloat((startingTax2) * counter2).toFixed(2);
     }
-  
+
 }
+
 function subtracter(v) {
     if(v===item1){
         counter1 -= 1;
-        v.innerHTML = counter1
+        v.innerHTML = counter1;
+        itemPrice1.innerHTML = 
+        parseFloat(itemPrice1.innerHTML - startingPrice1 ).toFixed(2);
+        itemTax1.innerHTML = 
+        parseFloat(itemTax1.innerHTML - startingTax1 ).toFixed(2);
     }else{
         counter2 -= 1;
         v.innerHTML = counter2;
+        itemPrice2.innerHTML = 
+        parseFloat(itemPrice2.innerHTML - startingPrice2    ).toFixed(2);
+        itemTax2.innerHTML = 
+        parseFloat(itemTax2.innerHTML - startingTax2   ).toFixed(2);
     }
 }
 
 
+// function updatePrices(v) {
+
+//         document.querySelector(".itemP1").innerHTML = "100.99";
+// }
+
+// Event Handlers
 addBtn1.addEventListener("click", function(){
     adder(item1)
+    // updatePrices(item1)
 });
 addBtn2.addEventListener("click", function(){
     adder(item2)
@@ -44,9 +79,3 @@ minusBtn2.addEventListener("click", function(){
     subtracter(item2)
 });
 
-console.log(item2)
-
-
-function updatePrices() {
-    item
-}
